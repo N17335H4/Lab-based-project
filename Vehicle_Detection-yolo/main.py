32583b370070e533b079cb512afb6a8596fec78d
1341/\/3 /\/|7335|-|4 <bniteesha@cs.iitr.ac.in>
2021-05-02 11:54:52 +0530

    integrated time slots

--- Vehicle_Detection-yolo/main.py
+++ Vehicle_Detection-yolo/main.py
@@ -1,10 +1,97 @@
 import get_traffic_count as gtc
+import get_traffic_slots as gts
 import specify_roi as roi
 import cv2 as cv
 import os
 import multiprocessing
 import time
-import enum
+import matplotlib.pyplot as plt
+
+def show_bar_plots(time, baseTimer, list):
+    
+    nlanes = len(list)
+
+    l=[]
+    l.append('g')
+    for i in range(nlanes -1):
+        l.append('r')
+    if nlanes == 3:
+        lanes = ['lane1','lane2','lane3']
+    elif nlanes == 4:
+        lanes = ['lane1','lane2','lane3','lane4']
+    tlzero = []
+    tlzero.append(list[0])
+    tlzero.append(list[0])
+    tlzero.append(list[0] + list[1])
+    if nlanes == 4:
+        tlzero.append(list[0] + list[1] + list[2])
+    plt.xlabel("Lanes")
+    plt.ylabel("Light Color")
+    plt.title("Traffic Signals after " + str(time) + " seconds: ")
+    plt.bar(lanes, tlzero, color=l)
+    plt.show()
+
+    #plotted next signal timers
+    lone=[]
+    lone.append('r')
+    lone.append('g')
+    for i in range(nlanes -2):
+        lone.append('r')   
+    count = list[0]
+    tlone=[]
+    tlone.append(baseTimer - count)
+    tlone.append(list[1])
+    tlone.append(list[1])
+    if nlanes == 4:
+        tlone.append(list[2] + list[1])
+    # print("After ", count, " seconds: ")
+    plt.xlabel("Lanes")
+    plt.ylabel("Light Color")
+    plt.title("Traffic Signals after "+ str(time+count)+ " seconds:")
+    plt.bar(lanes, tlone, color=lone)
+    plt.show()
+
+    #plotted next signal timers
+    ltwo=[]
+    ltwo.append('r')
+    ltwo.append('r')
+    ltwo.append('g')
+    if nlanes == 4:
+        ltwo.append('r')    
+    count = list[0] + list[1]
+    ttwo=[]
+    ttwo.append(baseTimer - count)
+    ttwo.append(baseTimer - list[1])
+    ttwo.append(list[2])
+    if nlanes == 4:
+        ttwo.append(list[2])
+    # print("After ", count, " seconds: ")
+    plt.xlabel("Lanes")
+    plt.ylabel("Light Color")
+    plt.title("Traffic Signals after "+ str(time+count)+ " seconds:")
+    plt.bar(lanes, ttwo, color=ltwo)
+    plt.show()
+
+    #plotted next signal timers
+    if nlanes == 4:
+        lth=[]
+        lth.append('r')
+        lth.append('r')
+        lth.append('r')
+        lth.append('g')
+        count = list[0] + list[1] + list[2]
+        tth=[]
+        tth.append(baseTimer - count)
+        tth.append(baseTimer - list[1] - list[2])
+        tth.append(baseTimer - list[2])
+        tth.append(list[3])
+        # print("After ", count, " seconds: ")
+        plt.xlabel("Lanes")
+        plt.ylabel("Light Color")
+        plt.title("Traffic Signals after "+ str(time+count)+ " seconds:")
+        plt.bar(lanes, tth, color=lth)
+        plt.show()
+
 
 # class traffic_light(enum.Enum):
 #     red = 1
@@ -21,11 +108,13 @@ caps = [None]*len(lanes)
 fpss = []
 durations = []
 masks = []
+lane_names = []
 for i in range(len(lanes)):
     
     caps[i] = cv.VideoCapture(lanes[i])
     name = os.path.basename(lanes[i])
     name = os.path.splitext(name)[0]
+    lane_names.append(name)
     mask_image = "mask images/mask_"+name+".png"
 
     fps = caps[i].get(cv.CAP_PROP_FPS)
@@ -73,8 +162,11 @@ while (all(x > t for x in durations)):
     end = time.time()
     print("Time taken is", float(end-start))
     print(counts)
-    for i in range(len(lanes)):
-        cv.imshow("output_"+str(i+1),images[i])
-        cv.waitKey()
-    cv.destroyAllWindows()
-    t+=20
+    a,time_slots = gts.get_traffic_slots(counts)
+    print("Time Slots:" ,time_slots)
+    show_bar_plots(t, a, time_slots)
+#     for i in range(len(lanes)):
+#         cv.imshow("output_"+str(i+1),images[i])
+#         cv.waitKey()
+#     cv.destroyAllWindows()
+    t+=a
